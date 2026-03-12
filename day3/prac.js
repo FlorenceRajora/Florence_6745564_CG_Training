fbf5f471020eda43351e4c76c388abdd454adf8b
Florence <harshitarajora15@gmail.com>
2026-03-12 23:48:49 +0530

    Clean repo and add gitignore

--- day3/prac.js
+++
@@ -1,8 +0,0 @@
-function demo(){
-    let a = 10;
-    var b = 20;
-    const c = 30;
-    console.log(a);
-}
-demo();
-// console.log(a);
--- day7/p1/tests/t1.spec.ts
+++
@@ -1,6 +0,0 @@
-import {test} from "@playwright/test"
-
-test("hiii",()=>{
-    console.log("hii");
-    
-})
